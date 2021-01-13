a98a564979d5d4c01a4d692ef798bd51c8bf5fec
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-13 09:15:52 -0600

    comunication components

---
+++ src/components/AccionSaldo.vue
@@ -0,0 +1,44 @@
+<template>
+  <button @click="accion" :disabled="disabled">{{texto}}</button>
+</template>
+
+<script>
+export default {
+    props:{
+        texto:String,
+        disabled: {
+            type:Boolean,
+            default:false
+        }
+    },
+    methods:{
+        accion(){
+            this.$emit("accion")
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+display: inline-block;
+padding: 15px 25px 10px 10px;
+margin: 10px 10px 10px 10px;
+font-size: 24px;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+outline: none;
+color: white;
+background-color: CORAL;
+border: none;
+border-radius: 15px;
+box-shadow: 0 10px #999;
+}
+button:hover {background-color: coral}
+button:active {
+background-color: coral;
+box-shadow: 0 5px #666;
+transform: translateY(4px);
+}
+</style>
--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -5,19 +5,52 @@
     <div v-for="(servicio, index) in servicios" :key="index">
       {{index}} -- {{ servicio }}
     </div>
+
+    <AccionSaldo 
+        texto="Aumentar Saldo"
+        @accion="aumentar"
+        
+    />
+    <AccionSaldo 
+        texto="Disminuir Saldo"
+        @accion="diminuir"
+        :disabled="disabled"
+    />
 </template>
 
 <script>
+import AccionSaldo from './AccionSaldo.vue'
 export default {
+    components:{
+        AccionSaldo,
+    },
     data() {
         return {
             saldo: 1000,
             cuenta: "visa",
             estado:true,
             servicios: ["giro","abono","trasferencia"],
+            disabled:false,
 
         }
     },
+    methods:{
+        aumentar(){
+            this.saldo= this.saldo+100;
+            this.disabled=false;
+        },
+        diminuir(){
+            if(this.saldo===0){
+                this.disabled=true;
+                alert("Saldo agotado")
+            }
+            else{
+                 
+                this.saldo= this.saldo-100;
+            }
+        }
+    }
+
 }
 </script>
 
